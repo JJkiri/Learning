5493e90361515b3c368a1819c665689f2bfb39f0
JJkiri <wkl9792@gmail.com>
2026-03-26 22:07:26 +0900

    260325

---
+++ SQL/lv4_온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,12 @@
+
+SELECT to_char(sales_date,'yyyy-mm-dd') as sales_date, product_id, user_id, sales_amount
+from ONLINE_SALE 
+where to_char(sales_date,'yyyy-mm-dd') like '2022-03%' 
+
+union all
+
+SELECT to_char(sales_date,'yyyy-mm-dd') as sales_date, product_id, null as user_id, sales_amount
+from OFFLINE_SALE 
+where  to_char(sales_date,'yyyy-mm-dd') like '2022-03%'
+
+order by sales_date asc, product_id asc, user_id asc
